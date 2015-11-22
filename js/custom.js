9535c8cc09bf4bd0f0760719dc932a6ee1e82f9d
elliot <postsneakernet@gmail.com>
2015-11-22 13:41:45 -0500

    Fix links

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,19 @@
-//$(function() {
-//    var sections = {},
-//        _height  = $(window).height(),
-//        i        = 0;
-//
-//    // Grab positions of our sections
-//    $('.section').each(function() {
-//        sections[this.name] = $(this).offset().top;
-//    });
-//
-//    $(document).scroll(function() {
-//        var $this = $(this),
-//            pos   = $this.scrollTop();
-////        
-////        for (i in sections) {
-////            if (sections[i] > pos -100 && sections[i] < pos + _height) {
-////                console.log(i);
-////                $('.elliot li').removeClass('active');
-////                $('#nav_' + i).addClass('active');
-////                
-////                if (i == "projects") {
-////                    console.log("hey projects section!!");
-////                }
-////                
-////                break;
-////            }
-////        }
-//
+$(function() {
+    var sections = {},
+        _height  = $(window).height(),
+        i        = 0;
+
+    // Grab positions of our sections
+    $('.section').each(function() {
+        sections[this.name] = $(this).offset().top;
+    });
+
+    $(document).scroll(function() {
+        var $this = $(this),
+            pos   = $this.scrollTop();
+//        
 //        for (i in sections) {
-//            if (sections[i] > pos && sections[i] < pos + _height) {
+//            if (sections[i] > pos -100 && sections[i] < pos + _height) {
 //                console.log(i);
 //                $('.elliot li').removeClass('active');
 //                $('#nav_' + i).addClass('active');
@@ -39,8 +25,22 @@
 //                break;
 //            }
 //        }
-//    });
-//});
+
+        for (i in sections) {
+            if (sections[i] > pos -100 && sections[i] < pos + _height) {
+                console.log(i);
+                $('.elliot li').removeClass('active');
+                $('#nav_' + i).addClass('active');
+                
+                if (i == "projects") {
+                    console.log("hey projects section!!");
+                }
+                
+                break;
+            }
+        }
+    });
+});
 
 //
 //$('.elliot a').click(function(event) {
