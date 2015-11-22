38bd5e3ee84b0e4c476b3a73f883dc58c3d570ed
elliot <postsneakernet@gmail.com>
2015-11-22 14:00:14 -0500

    Fix links

--- js/custom.js
+++ js/custom.js
@@ -11,20 +11,7 @@ $(function() {
     $(document).scroll(function() {
         var $this = $(this),
             pos   = $this.scrollTop();
-//        
-//        for (i in sections) {
-//            if (sections[i] > pos -100 && sections[i] < pos + _height) {
-//                console.log(i);
-//                $('.elliot li').removeClass('active');
-//                $('#nav_' + i).addClass('active');
-//                
-//                if (i == "projects") {
-//                    console.log("hey projects section!!");
-//                }
-//                
-//                break;
-//            }
-//        }
+
 
         for (i in sections) {
             if (sections[i] > pos -100 && sections[i] < pos + _height) {
@@ -42,12 +29,12 @@ $(function() {
     });
 });
 
-//
-//$('.elliot a').click(function(event) {
-//    console.log("clicked");
-//
-//    $('.elliot li').removeClass('active');
-//    var par = $(event.target).parent().get(0).tagName;
-//    console.log("event is: " + par);
-//    $(event.target).parent().addClass("active");
-//});
+
+$('.elliot a').click(function(event) {
+    console.log("clicked");
+
+    $('.elliot li').removeClass('active');
+    var par = $(event.target).parent().get(0).tagName;
+    console.log("event is: " + par);
+    $(event.target).parent().addClass("active");
+});
